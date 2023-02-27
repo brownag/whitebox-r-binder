0251fabcb156fe70513b043d54e630dd7b833866
Andrew Gene Brown <brown.andrewg@gmail.com>
2023-02-26 17:29:15 -0800

    Update install.R

--- binder/install.R
+++ binder/install.R
@@ -2,5 +2,5 @@ options(repos = c(getOption("repos"),
                   "https://RcppCore.github.io/drat", 
                   "https://cloud.r-project.org",
                   "http://rspatial.r-universe.dev/"))
-install.packages(c("devtools", "Rcpp", "terra", "rmarkdown", "httr", "caTools", "rprojroot", "ggplot2", "whitebox"))
+install.packages(c("devtools", "Rcpp", "terra", "rmarkdown", "httr", "caTools", "rprojroot", "ggplot2", "tidyterra", "whitebox"))
 remotes::install_github("giswqs/whiteboxR")
